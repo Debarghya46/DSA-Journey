eabc982b9d3404c198948909e7a52b3ee1d7eaf4
Debarghya46 <debarghya4632@gmail.com>
2026-08-25 01:02:45 +0530

    Time: 7 ms (79.86%), Space: 46.9 MB (84.89%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for (int i = 0; i < half; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                q1++;
+            } else {
+                sum1 += ch - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                q2++;
+            } else {
+                sum2 += ch - '0';
+            }
+        }
+
+        // Odd number of '?' means Alice gets the advantage
+        if ((q1 + q2) % 2 == 1) {
+            return true;
+        }
+
+        // Bob wins only when the sums can be perfectly balanced
+        return sum1 - sum2 != 9 * (q2 - q1) / 2;
+    }
+}
